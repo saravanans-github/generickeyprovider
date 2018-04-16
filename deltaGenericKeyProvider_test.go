ad4cab083fddb749e4ad376e4819671b9e2d590d
Saravanan <saravanans.github@outlook.sg>
2018-04-16 23:18:25 +0800

    updated unit tests for multiple kids

--- deltaGenericKeyProvider_test.go
+++ deltaGenericKeyProvider_test.go
@@ -9,7 +9,7 @@ func TestGenerateWidevinePssh(t *testing.T) {
 
 	contentIdInBin := []byte("test123")
 	keyIdInBin := encode.HexStringToBin("eee405eddea64f378e51ec167eca8d33")
-	data, err := generateWidevinePssh(keyIdInBin, contentIdInBin, "widevine_test", "SD")
+	data, err := generateWidevinePssh([][]byte{keyIdInBin}, contentIdInBin, "widevine_test", "SD")
 
 	t.Log(encode.BytesToBase64(data))
 
@@ -21,9 +21,9 @@ func TestGenerateWidevinePssh(t *testing.T) {
 func TestGenerateMp4Pssh(t *testing.T) {
 	contentIdInBin := []byte("test123")
 	keyIdInBin := encode.HexStringToBin("eee405eddea64f378e51ec167eca8d33")
-	widevinePssh, _ := generateWidevinePssh(keyIdInBin, contentIdInBin, "widevine_test", "SD")
+	widevinePssh, _ := generateWidevinePssh([][]byte{keyIdInBin}, contentIdInBin, "widevine_test", "SD")
 
-	mp4Pssh, err := generateMp4Pssh(keyIdInBin, "edef8ba979d64acea3c827dcd51d21ed", widevinePssh)
+	mp4Pssh, err := generateMp4Pssh([][]byte{keyIdInBin}, "edef8ba979d64acea3c827dcd51d21ed", widevinePssh)
 	if encode.BytesToBase64(mp4Pssh) != "AAAAXnBzc2gBAAAA7e+LqXnWSs6jyCfc1R0h7QAAAAHu5AXt3qZPN45R7BZ+yo0zAAAAKhIQ7uQF7d6mTzeOUewWfsqNMxoNd2lkZXZpbmVfdGVzdCIHdGVzdDEyMw==" || err != nil {
 		t.FailNow()
 	}
