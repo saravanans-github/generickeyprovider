01c635db41d9bfcd2b9497c99b0032bc272f5e28
Saravanan <saravanans.github@outlook.sg>
2018-04-16 22:30:46 +0800

    added unit test for mp4pssh generator

--- deltaGenericKeyProvider_test.go
+++ deltaGenericKeyProvider_test.go
@@ -5,14 +5,26 @@ import (
 	"testing"
 )
 
-func TestGenerateProtoBuf(t *testing.T) {
+func TestGenerateWidevinePssh(t *testing.T) {
 
-	data, err := generateProtoBuf(encode.HexStringToBin("79221c6e568d6dbec38e2eac02d299cd"),
-		[]byte("key-id:eSIcblaNbb7Dji6sAtKZzQ=="),
-		"widevine_test",
-		"SD")
+	contentIdInBin := []byte("test123")
+	keyIdInBin := encode.HexStringToBin("eee405eddea64f378e51ec167eca8d33")
+	data, err := generateWidevinePssh(keyIdInBin, contentIdInBin, "widevine_test", "SD")
 
-	if encode.BytesToBase64(data) != "CAESEHkiHG5WjW2+w44urALSmc0aDXdpZGV2aW5lX3Rlc3QiH2tleS1pZDplU0ljYmxhTmJiN0RqaTZzQXRLWnpRPT0qAlNEMgA=" || err != nil {
+	t.Log(encode.BytesToBase64(data))
+
+	if encode.BytesToBase64(data) != "EhDu5AXt3qZPN45R7BZ+yo0zGg13aWRldmluZV90ZXN0Igd0ZXN0MTIz" || err != nil {
+		t.FailNow()
+	}
+}
+
+func TestGenerateMp4Pssh(t *testing.T) {
+	contentIdInBin := []byte("test123")
+	keyIdInBin := encode.HexStringToBin("eee405eddea64f378e51ec167eca8d33")
+	widevinePssh, _ := generateWidevinePssh(keyIdInBin, contentIdInBin, "widevine_test", "SD")
+
+	mp4Pssh, err := generateMp4Pssh(keyIdInBin, "edef8ba979d64acea3c827dcd51d21ed", widevinePssh)
+	if encode.BytesToBase64(mp4Pssh) != "AAAAXnBzc2gBAAAA7e+LqXnWSs6jyCfc1R0h7QAAAAHu5AXt3qZPN45R7BZ+yo0zAAAAKhIQ7uQF7d6mTzeOUewWfsqNMxoNd2lkZXZpbmVfdGVzdCIHdGVzdDEyMw==" || err != nil {
 		t.FailNow()
 	}
 }
